f589a3f1b3c5a6ba300d8cf8ec8799b507d41178
Campinho Digital <108235987+campinho-digital@users.noreply.github.com>
2024-03-14 18:06:03 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-var valorA;
-valorFinal = valorA;
-console.log(valorFinal);
+// O que acontece ao tentar reatribuir um valor a const idade? ou seja, criar uma segunda const idade = 30?
+const idade = 25;
+//console.log(idade); 
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+//O que acontece ao chamar a let x dentro e fora do escopo? 
+
+//Escopo refere-se ao contexto no qual as variáveis e funções são definidas e acessadas.
+//Nesse caso as chaves delimitam o nosso escopo
+
+{
+    let x = 10;
+   // console.log(x); 
+}
+// console.log(x);
+
+
+
+///////////////////////////////////////////////////////////////////////
+
+//Ainda sobre escopo explique a diferença dos console.log
+
+{
+    let x = 10;
+    var y = 20;
+}
+
+// console.log(x);
+// console.log(y);
+
+///////////////////////////////////////////////////////////////////////
+
+//O que acontece nessa declaração, explique sobre o Hoisting
+
+//console.log(a); 
+var a = 5;
+console.log(b);
+//let b = 10;
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//No código abaixo apresenta um erro, qual?
+
+var x = 5;
+var x = 10;
+// console.log(x); 
+
+let y = 15;
+let y = 20; 
+// console.log(y); 
+
+
+
